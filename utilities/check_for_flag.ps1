8dcdeb299d6c39f0e4a0d2ae19a73e3c0ab65745
reedsutliff <sutliff.reed@gmail.com>
2021-12-30 11:16:18 -0500

    added test-template, updated check_for_flag and deploy-template

--- utilities/check_for_flag.ps1
+++ utilities/check_for_flag.ps1
@@ -1,20 +1,39 @@
 param(
-    [string] $search_phrase
+    [string] $search_phrases,
+    [switch] $find_value = $false
 )
-
-#Get Most Recent Commit Message
-$phrase_found = "false"
+$phrase_found = $false
+$flag_value = "null"
 $commit_message = git log -1 --no-merges --pretty=%B
 
+Write-Host "Searching For Value? - $find_value"
 Write-Host "Most Recent Commit Message- $commit_message"
-Write-Host "Search Phrase - $search_phrase"
 
-$escaped_search_phrase = $search_phrase -replace "\]","\]" -replace "\[","\["
-Write-Host "Escaped Search Phrase - $escaped_search_phrase"
+$search_phrases.Split(",") | ForEach{
+    $flag = $_.Trim()
+    Write-Host "Search Flag - $flag"
+    if($find_value){
+        $search_phrase = "\[$flag\s?=\s?\'(.*?)\'\]"
+        Write-Host "Flag Regex - $search_phrase"
+        if("$commit_message" -Match "$search_phrase"){
+            $phrase_found = $true
+            $flag_value = $Matches[1].Trim()
+        }
+    }
+    else
+    {
+        $search_phrase = "\[$flag\]"
+        Write-Host "Flag Regex - $search_phrase"
+        if("$commit_message" -Match "$search_phrase"){
+            $phrase_found = $true   
+        }
+    }
 
-if("$commit_message" -Match "$escaped_search_phrase"){
-    $phrase_found = "true"    
 }
 
-Write-Host "Search Phrase Found? - $phrase_found"
+Write-Host "Search Flag(s) Found? - $phrase_found"
 Write-Host "##vso[task.setvariable variable=phraseFound;isOutput=true]$phrase_found"
+if($find_value -And $phrase_found){
+    Write-Host "Flag Value - $flag_value"
+    Write-Host "##vso[task.setvariable variable=flagValue;isOutput=true]$flag_value"  
+}
